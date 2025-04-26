c3238ab92cc724762dcf59752881cc718dd46ac0
edwincanopy <edn9@pm.me>
2025-04-26 14:26:37 -0700

    m

---
+++ speaker_embedding.py
@@ -0,0 +1,48 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import torch.nn as nn
+from pyannote.audio import Model, Inference
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llama = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-3B")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-3B")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+llama = llama.to(device)
+print(f"Using device: {device}")
+
+class GatedMLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        return x
+    
+
+model = Model.from_pretrained("pyannote/wespeaker-voxceleb-resnet34-LM")
+inference = Inference(model, window="whole")
+
+def embed_speaker(audio_path):
+    embedding = inference(audio_path)
+    return embedding
+
+
+class LlamaForSpeakerModeling(AutoModelForCausalLM):
+    def __init__(self, config):
+        super().__init__(config)
+        
+        self.projection = GatedMLP()
+
+    def forward(self, input_ids, attention_mask):
+        return
+
+
