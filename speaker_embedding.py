7235b975e5fab5bdea2cdc6944db27ba85ac2c1c
edwincanopy <edn9@pm.me>
2025-04-29 15:06:56 -0700

    m

--- speaker_embedding.py
+++ speaker_embedding.py
@@ -177,7 +177,7 @@ def collate_fn(batch):
     coll = default_data_collator(batch)
     coll["input_ids"] = torch.stack([torch.tensor(b["codes_list"]) for b in batch], dim=0)
     # coll["speaker_embedding"] = torch.stack([torch.tensor(b["speaker_embedding"]) for b in batch], dim=0)
-    # coll["text"] = [b["text"] for b in batch]
+    coll["text"] = [b["text"] for b in batch]
     return coll
 
 training_args = TrainingArguments(
