1c9ac3566a674fa79b539c0980a8a2d103d4dcc3
edwincanopy <edn9@pm.me>
2025-05-02 14:34:56 -0700

    m

--- concat.py
+++ concat.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset, concatenate_datasets
-
+from huggingface_hub import snapshot_download
 
 all_ds = []
 
 for i in range(10):
-    ds = load_dataset("edwindn/voice_cloning_dataset_0", split=f"train_{i}")
+    snapshot_download(repo_id=f"edwindn/voice_cloning_dataset_{i}", repo_type="dataset", num_workers=os.cpu_count())
+    ds = load_dataset(f"edwindn/voice_cloning_dataset_{i}", split=f"train")
     all_ds.append(ds)
 
 all_ds = concatenate_datasets(all_ds)
